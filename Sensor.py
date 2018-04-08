79da175d74a6b72132f850f01e81adca31ab780d
Piotr <3ponichtera@fis.agh.edu.pl>
2018-04-08 20:48:12 +0200

    Added command line arguments

--- Sensor.py
+++ Sensor.py
@@ -5,6 +5,12 @@ import time
 import socket
 import requests
 import json
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("measurement", help="= cpu, memory or disk")
+args = parser.parse_args()
+data = {}
 
 #########GET MAC and IP#############
 mac_addresses = []
@@ -19,55 +25,65 @@ mac_address = mac_addresses[0]
 myip = requests.get('http://ip.42.pl/raw').text
 
 ##########GET CPU (%)###############
-all_cpus_usage=0
-time.sleep(1)
-cpus_count = psutil.cpu_count()
-cpus_percent = psutil.cpu_percent(percpu=True)
-for percent in cpus_percent:
-    all_cpus_usage += float(percent)
+def getCPU():
+    all_cpus_usage=0
+    time.sleep(1)
+    cpus_count = psutil.cpu_count()
+    cpus_percent = psutil.cpu_percent(percpu=True)
+    for percent in cpus_percent:
+        all_cpus_usage += float(percent)
 
-all_cpus_usage = all_cpus_usage / cpus_count
-all_cpus_usage = "%.2f" % (all_cpus_usage)
+    all_cpus_usage = all_cpus_usage / cpus_count
+    all_cpus_usage = "%.2f" % (all_cpus_usage)
+    return all_cpus_usage
 
 #########GET RAM (MB)#############
-ram = psutil.virtual_memory()
+def getRAM():
+    ram = psutil.virtual_memory()
 
-total_ram = "%.2f" % (ram.total / pow(1024,2))
-used_ram = "%.2f" % (ram.used / pow(1024,2))
+    total_ram = "%.2f" % (ram.total / pow(1024,2))
+    used_ram = "%.2f" % (ram.used / pow(1024,2))
+    return total_ram, used_ram
 
 #########GET MEMORY (MB)##########
-disk = psutil.disk_partitions(all=False)
+def getMemory():
+    disk = psutil.disk_partitions(all=False)
 
-for part in psutil.disk_partitions(all=False):
-    if os.name == 'nt':
-        if 'cdrom' in part.opts or part.fstype == '':
-            continue
-    usage = psutil.disk_usage(part.mountpoint)
-    break
+    for part in psutil.disk_partitions(all=False):
+        if os.name == 'nt':
+            if 'cdrom' in part.opts or part.fstype == '':
+                continue
+        usage = psutil.disk_usage(part.mountpoint)
+        break
 
-total_disk = "%.2f" % (usage.total / pow(1024,3))
-used_disk = "%.2f" % (usage.used / pow(1024,3))
+    total_disk = "%.2f" % (usage.total / pow(1024,3))
+    used_disk = "%.2f" % (usage.used / pow(1024,3))
+    return total_disk, used_disk
 
-###########JSON###################
-data = {}
+#########GET DATA################
 data['MAC'] = mac_address
 data['IP'] = myip
-data['CPU'] = all_cpus_usage
-data['RAM_TOTAL'] = total_ram
-data['RAM_USED'] = used_ram
-data['DISK_TOTAL'] = total_disk
-data['DISK_USED'] = used_disk
 
-json_data = json.dumps(data)
-print(json_data)
+if args.measurement == 'cpu':
+    data['CPU'] = getCPU()
+elif args.measurement == 'disk':
+    data['DISK_TOTAL'] = getMemory()[0]
+    data['DISK_USED'] = getMemory()[1]
+elif args.measurement == 'memory':
+    data['RAM_TOTAL'] = getRAM()[0]
+    data['RAM_USED'] = getRAM()[1]
+else:
+    print("Wrong argument")
+    sys.exit()
 
 #########SEND JSON###############
-#TODO
-#url = ''
-#payload = json_data
-#headers = {'content-type': 'application/json'}
+json_data = json.dumps(data)
 
-#response = requests.post(url, data=json.dumps(payload), headers=headers)
-#response.status_code
+url = 'http://127.0.0.1:54321/measurement'
+payload = json_data
+headers = {'content-type': 'application/json'}
+
+response = requests.post(url, data=json.dumps(payload), headers=headers)
+
 
 
